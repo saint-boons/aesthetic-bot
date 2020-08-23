88f744c168681d3b5bee74e6460955ad46468b76
frenchbones <hugo.bastien_@outlook.com>
2020-08-23 18:54:12 +0200

    v0.0.2
    Fixed a couple of bugs and added .gitignore

--- commands/avatar.js
+++ commands/avatar.js
@@ -9,9 +9,7 @@ module.exports = {
         const avatarList = message.mentions.users.map(user => {
             return `**${user.username}'s avatar:** ${user.displayAvatarURL({ format: "png", dynamic: true })}`;
         });
-    
-        // send the entire array of strings as a message
-        // by default, discord.js will `.join()` the array with `\n`
+        
         message.channel.send(avatarList);
 	},
 };
--- commands/bot-info.js
+++ commands/bot-info.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'bot-info',
 	description: 'Displays info about the bot.',
 	execute(message, args) {
-		message.channel.send(`**Bot name:** ${message.client.user}\n**Server Count:** ${message.client.guilds.cache.size}\n**Uptime:** ${message.client.uptime / 100}s\n**Bot Creator:** FrenchBones (<https://discord.gg/7T24Mrw>, <https://frenchbones.github.io>)`);
+		message.channel.send(`**Bot name:** ${message.client.user}\n**Server Count:** ${message.client.guilds.cache.size}\n**Uptime:** ${message.client.uptime / 1000}s *(${message.client.uptime / 60000}m)*\n**Bot Creator:** FrenchBones (<https://discord.gg/7T24Mrw>, <https://frenchbones.github.io>)`);
 	},
 };
