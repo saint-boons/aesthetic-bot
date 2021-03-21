20196208d40aeff1908d233c4d4b9645a638b14a
frenchbones <38706314+frenchbones@users.noreply.github.com>
2021-03-21 21:17:05 +0100

    0.1.7

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,5 @@
 const { prefix } = require('../../config.json');
+
 module.exports = {
     name: 'help',
     description: 'List all of my commands or info about a specific command.',
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,6 +5,19 @@ module.exports = {
     usage: 'no arguments',
     guildOnly: false,
 	execute(message, args) {
-		message.channel.send('**Pong.**');
+		const ping = resultMessage.createdTimestamp - message.createdTimestamp
+		const pingEmbed = new Discord.MessageEmbed()
+			.setColor(embedColor)
+			.setTitle('Ping Results')
+			.setDescription('Here are the detailed ping results!')
+			.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+			.addFields(
+				{ name: 'BOT Latency', value: `${ping} ms`, inline: true },
+				{ name: 'API Latency', value: `${client.ws.ping} ms`, inline: true },
+			)
+			.setImage('https://i.imgur.com/wSTFkRM.png')
+			.setFooter(embedFooterText, embedFooterIcon);
+
+		message.channel.send(pingEmbed);
 	},
 };
