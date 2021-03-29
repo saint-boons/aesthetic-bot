54105e6e8feafb49424624c6a80dac45080cd920
frenchbones <38706314+frenchbones@users.noreply.github.com>
2021-03-29 23:29:59 +0200

    0.2.8

--- commands/help.js
+++ commands/help.js
@@ -1,62 +1,33 @@
-const Discord = require('discord.js');
-const config = require('../config.json')
-const loadCommands = require('./load-commands')
+const fs = require("fs");
+const path = require("path");
+const Discord = require("discord.js");
+const config = require("../config.json");
+const loadCommands = require("./load-commands");
 
 module.exports = {
-	commands: ['help'],
+	commands: ["help"],
+	expectedArgs: '<command> / none',
+	maxArgs: 1,
 	description: "Gives a list of avaliable commands and more info about them.",
 	callback: (client, message, arguments, text) => {
-		const helpEmbed = new Discord.MessageEmbed()
-			.setColor(config.embedColor)
-			.setTitle('Command Help')
-			.setDescription('Here is a list of all avaliable commands.')
-			.setFooter(config.embedFooterText, config.embedFooterIcon);
-		const commands = loadCommands()
-
-		for (const command of commands) {
-			// Perm check
-			let permissions = command.permissions
-			if (permissions) {
-				let hasPermission = true
-				if (typeof permissions === 'string') {
-					permissions = [permissions]
-				}
-				for (const permission of permissions) {
-					if (!message.member.hasPermission(permission)) {
-						hasPermission = false
-						break
-					}
-				}
-				if (!hasPermission) {
-					continue
-				}
+		const title = (str) => str.replace(/\b\S/g, (t) => t.toUpperCase());
+		if (!arguments[0]) {
+			const helpEmbed = new Discord.MessageEmbed()
+				.setColor(config.embedColor)
+				.setTitle("Command Help")
+				.setDescription("Here are the avaliable commands.")
+				.setFooter(config.embedFooterText, config.embedFooterIcon);
+			const commandsFiles = fs.readdirSync(path.join(__dirname, './'));
+			const folders = commandsFiles.filter(command => !command.includes('.js'));
+			for (const folder of folders) {
+				const commandList = fs.readdirSync(path.join(__dirname, '.', folder));
+				const commandListFormat = commandList.join(', ').replace(/\.js/g, '');
+				console.log(commandListFormat)
+				helpEmbed.addField(title(folder), `\`\`\`${commandListFormat}\`\`\``, true)
 			}
-			// Role check
-			let requiredRoles = command.requiredRoles
-			if (requiredRoles) {
-				let hasRequiredRole = true
-				if (typeof requiredRoles === 'string') {
-					requiredRoles = [requiredRoles]
-				}
-				for (const requiredRole of requiredRoles) {
-					if (!message.member.roles.cache.has(requiredRole)) {
-						hasRequiredRole = false
-						break
-					}
-				}
-				if (!hasRequiredRole) {
-					continue
-				}
-			}
-
-			// Format
-			const mainCommand = typeof command.commands === 'string' ? command.commands : command.commands[0]
-			const args = command.expectedArgs ? ` ${command.expectedArgs}` : ''
-			const { description } = command
-			const title = str => str.replace(/\b\S/g, t => t.toUpperCase());
-			const mainCommandTitle = title(mainCommand)
-			helpEmbed.addField(`${mainCommandTitle}`, `\`${config.prefix}${mainCommand}${args}\`\n*${description}*`, false)
+			message.channel.send(helpEmbed);
+		} else {
+			//
 		}
-		message.channel.send(helpEmbed);
 	},
 };
